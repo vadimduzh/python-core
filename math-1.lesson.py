4de77f17e53d1bc4505ee0a928392561bbd8ba5e
vadimduzh <vadim.duzh@gmail.com>
2023-06-04 21:46:30 +0300

    implemented

---
+++ math-1.lesson.py
@@ -0,0 +1,9 @@
+# comments - CTRL + /
+# print("Result: ", 5**5000)
+# print ("Result: ", 5, 10, sep="|", end="\n")
+# print("Result", min(1 , 2, 3, 4, 5, 6, 7))
+#  abs - modul
+# pow(**)
+# round - округление
+
+input("Enter your name: ")
