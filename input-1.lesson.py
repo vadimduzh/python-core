85716225c452e42bd375e59e0c0801df8d8fc492
vadimduzh <vadim.duzh@gmail.com>
2023-06-08 09:11:50 +0300

    implemented

--- input-1.lesson.py
+++ input-1.lesson.py
@@ -1,1 +1,14 @@
-number = 5
+number = 5 # int
+
+digit = -40000 # float
+word = "Result" # string
+boolean = True # bool
+
+str_num = "5" # string
+
+# print(word + str(digit))
+
+print(word + str(number + bool))
+
+del number
+
