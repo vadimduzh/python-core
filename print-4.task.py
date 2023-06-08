3613a0495a869b9ac2393d899bb54d40234cf9e9
vadimduzh <vadim.duzh@gmail.com>
2023-06-08 10:05:54 +0300

    Done

--- print-4.task.py
+++ print-4.task.py
@@ -2,4 +2,7 @@
 # 1. Вывести Python Works! в одну строку на экране
 # 2. Вывести First line в первой строке, а Second line во второй строке
 
+print("Python works")
+print("First line\nSecond line")
+
 
