751e3ee9d32749768fa1f2911a0fdca931f2f4e3
vadimduzh <vadim.duzh@gmail.com>
2023-06-09 18:50:54 +0300

    Done

--- if-1.task.py
+++ if-1.task.py
@@ -1,2 +1,9 @@
 # - Пользователь вводит 2 числа и сохраняет в переменных a и b (не забудь про int())
 # - Если a равно b, тогда на экран выводим текст a равно b, иначе выдоим a не равно b
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if a == b:
+    print("a = b")
+else:
+    print("a != b")
