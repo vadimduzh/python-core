0389b36bd9c455e28617fb665fb452678d2f3c97
vadimduzh <vadim.duzh@gmail.com>
2023-06-09 18:18:52 +0300

    Done

--- input-2.task.py
+++ input-2.task.py
@@ -1,7 +1,21 @@
 # - Попросить пользователя ввести сторону квадрата и сохранить в переменной ls
+
+ls = int(input("Enter the side of the square: "))
+print(ls)
+
 # - Преобразовать значение переменной ls в число (использовать функцию int) и сохранить в переменной l
+
+l = int(ls)
+print(l)
+
 # - Посчитать площадь квадрата и сохранить переменной res
+
+side = int(input("Enter the side of the square: "))
+S = side * side
+
 # - Вывести площадь res на экран
+res = S
+print(res)
 
 # https://youtu.be/DZvNZ9l9NT4?t=893 - тут можно посмотреть теорию
 
