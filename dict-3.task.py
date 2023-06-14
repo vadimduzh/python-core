1011100c59a69ccb05a8bc2a032c5c0075b3bd40
vduzh <vduzh@tut.by>
2023-06-14 11:48:15 +0300

    Уроки Python с нуля / #10 – Словари (dict) и работа с ними

---
+++ dict-3.task.py
@@ -0,0 +1,11 @@
+# Дан словарь car. Вывести на экран все ключи и для каждого ключа его значение:
+# brand - Ford
+# model - Mustang
+# year - 1964
+print("= START =")
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
