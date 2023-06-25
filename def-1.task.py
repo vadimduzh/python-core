6b9122cc881c21e7f52bbe67d027a75b920a08e8
vadimduzh <vadim.duzh@gmail.com>
2023-06-25 12:12:34 +0300

    Done

--- def-1.task.py
+++ def-1.task.py
@@ -1,23 +1,23 @@
 # Написать функцию func1, которая ничего не делает (т.е. пустая). Использовать pass
 # Вызвать функцию func1
-# def func1():
-#     pass
+def func1():
+    pass
 
 
 # Написать функцию func2, которая печатает Hello World! (использовать print внутри функции)
 # Вызвать функцию func2
 
-# def func2():
-#     print("Hello world")
-#
-# func2()
+def func2():
+    print("Hello world")
+
+func2()
 
 # Написать функцию func3, которая принимает параметр name и печатает Hello, а потом то, что передается в name
 # Вызвать функцию func3 с параметром John
-# def func3(name):
-#     print("Hello", name)
-#
-# func3("John")
+def func3(name):
+    print("Hello", name)
+
+func3("John")
 
 
 # Написать функцию func4, которая
@@ -27,9 +27,9 @@
 # Вызвать функцию func4 с параметрами (4, 5) и сохранить результат в переменной value
 # Вывести на экран value (должно быть 10)
 
-#
-# def func4(a, b):
-#     res = a * b
-#     print(res)
-#
-# func4(4, 5)
+
+def func4(a, b):
+    res = a * b
+    print(res)
+
+func4(4, 5)
