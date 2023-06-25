49320de6c77bd291dc092d181e022ffd0cb076dd
vadimduzh <vadim.duzh@gmail.com>
2023-06-25 12:58:10 +0300

    Done

--- dict-1.task.py
+++ dict-1.task.py
@@ -1,10 +1,12 @@
 # - создать словарь d, c ключом name и значением "John".
 # - Вывести d на экран.
-#
-print("= START =")
-# d =
+d = {"name": "John"}
+print(d["name"])
 
 # - создать словарь d, c ключом name и значением "John", ключом age и значение 15, с ключом male и значением True.
 # - Вывести d на экран.
-print("= START =")
-# d =
+d = {"name": "John", "age": 15, 'male': True}
+print(d)
+
+
+
