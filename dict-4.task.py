75fc2455dbebb92bf37be0b6caad8bb46c0aa1c8
vadimduzh <vadim.duzh@gmail.com>
2023-06-28 22:18:23 +0300

    Done

--- dict-4.task.py
+++ dict-4.task.py
@@ -1,13 +1,21 @@
 # Создать словарь student с ключами name, phone, born, height
 # Вывести на экран словарь student:
-print("= START =")
-
-
+student = {
+    "name": "Vadim",
+    "phone": "375293957039",
+    "born": "2008",
+    "height": "187"
+}
+print(student)
 # Создать словарь student (изначально без ключей) и создать ключи со значениями (использовать []):
 # name - "John"
 # phone - "111-22-33"
 # born - "20-06-2008"
 # height - 186
 # Вывести на экран словарь student:
-print("= START =")
 student = {}
+student["name"] = "John"
+student["phone"] = "111-22-33"
+student["born"] = "20-06-2008"
+student["height"] = "186"
+print(student)
