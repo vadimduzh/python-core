7c9807f7effd94a99480378cc13c29817e16c9f6
vadimduzh <vadim.duzh@gmail.com>
2023-07-07 19:55:40 +0300

    Done

--- list-4.task.py
+++ list-4.task.py
@@ -4,57 +4,54 @@
 # - Создать список.
 # - Используя функцию append добавить в список элементы 0, 1, 2, ..., num -1
 # - Вывести список на экран
-lst = []
-num = int(input("Enter the number: "))
-# ERROR: используй for
-lst.append(0)
-lst.append(1)
-lst.append(2)
-lst.append(num)
-lst.append(-1)
-print(lst)
+# lst = []
+# num = int(input("Enter the number: "))
+# for i in range(0, num):
+#     lst.append(i)
+# print(lst)
 
 # - Пользователь вводит число с экрана и сохраняем значение в переменную num.
 # - Создать список.
 # - Используя функцию append добавить в список элементы 0, 1 * 10, 2 * 110, ..., (num -1) * 10
 # - Вывести список на экран
 # Code...
-
-lst = []
-num = int(input("Enter the number: "))
-for i in range(1, num):
-    i = i * 10
-    lst.append(i)
-print(lst)
+#
+# lst = []
+# num = int(input("Enter the number: "))
+# for i in range(1, num):
+#     i = i * 10
+#     lst.append(i)
+# print(lst)
 
 # - Дан список lst.
 # - Создать 2 новых списка lst1 и lst2
 # - lst1 содержит числа меньше 10
 # - lst2 содержит числа больше 10
 # - вывести lst1 и lst2 на экран.
-lst = [1, 10, 2, 3, 20, 30]
-lst1 = []
-lst2 = []
-for i in lst:
-    if i <= 10:
-        lst1.append(i)
-    else:
-        lst2.append(i)
-print(lst2)
-print(lst1)
+# lst = [1, 10, 2, 3, 20, 30]
+# lst1 = []
+# lst2 = []
+# for i in lst:
+#     if i <= 10:
+#         lst1.append(i)
+#     else:
+#         lst2.append(i)
+# print(lst2)
+# print(lst1)
 
 # - Дан список lst. Сложить первые 3 элемента в списке и добавить результат в lst
 # Подсказка:
 # - используем переменную total
 # - Проходим по первым 3-м элементам lst и складываем в total
 # - добавляем total в конец lst
+print("START")
+
 lst = [1, 20, 3, 40, 5]
 total = 0
 # ERROR: неправильно реализовано
-for i in lst:
-    if i <= 3:
-        del lst[i]
-        total = total + i
+for i in range(len(lst)):
+    if i < 3:
+        total = total + lst[i]
 lst.append(total)
 print(lst)
 
@@ -70,9 +67,10 @@ lst = [1, 2, 3, 4, 5]
 # Подсказка:
 # - После того, как посчитаешь total надо удалить первые 3 элемента
 # - Потом вставить (insert) в начало списка значение total (т.е. [6, 4, 5])
+print("START")
 lst = [10, 12, 30, 4, 50]
 total = 0
-# ERROR: неправильно реализовано - удаляй через pop и пользуй range
+
 for i in lst:
     if i <= 3:
         total = total + i
