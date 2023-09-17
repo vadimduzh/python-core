f3149d50c1d20c3443dfdd036a3fb96fe2d7c716
vadimduzh <Vadim.duzh@gmail.com>
2023-09-17 20:52:40 +0300

    Done

--- olymp-001.py
+++ olymp-001.py
@@ -1,7 +1,9 @@
 # Сложить 2 числа.
 
 # Числа задаются в файле data/olymp-001-in.txt
+
 # - считать с файла первое число в переменную s1, используя функцию readline
+
 # - считать с файла второе число в переменную s2, используя функцию readline
 
 # - преобразовать s1 в число num1
@@ -13,3 +15,22 @@
 # - записать s в файл tmp/olymp-001-out.txt
 
 # - зайти в файл tmp/olymp-001-out.txt и проверить результат
+
+
+file = open("data/olymp-001-in.txt", "r")
+
+s1 = file.readline()
+s1 = int(s1)
+s2 = file.readline()
+s2 = int(s2)
+
+res = s1 + s2
+s = str(res)
+
+file.close()
+
+file = open("tmp/olymp-001-out.txt", "w")
+
+file.write(s)
+
+file.close()
