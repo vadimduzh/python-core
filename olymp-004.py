267f11b159b873facd01672f027095a2ce1fa6c1
vadimduzh <Vadim.duzh@gmail.com>
2023-09-19 19:39:27 +0300

    Done

--- olymp-004.py
+++ olymp-004.py
@@ -1,2 +1,25 @@
 # В файле data/olymp-004-in.txt заданы числа.
 # Нужно перемножить все числа и результат записать в файл tmp/olymp-004-out.txt
+
+file = open("data/olymp-004-in.txt", "r")
+
+s1 = file.readline()
+num1 = int(s1)
+
+s2 = file.readline()
+num2 = int(s2)
+
+s3 = file.readline()
+num3 = int(s3)
+
+result = num1 * num2 * num3
+res = str(result)
+
+file.close()
+
+file = open("tmp/olymp-004-out.txt", "w")
+
+file.write(res)
+
+file.close()
+
