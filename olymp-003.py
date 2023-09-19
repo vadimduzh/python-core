53aadf72168b7b8cf1aec23471f7097ffac9785e
vadimduzh <Vadim.duzh@gmail.com>
2023-09-19 15:30:48 +0300

    Done

--- olymp-003.py
+++ olymp-003.py
@@ -4,5 +4,26 @@
 # Подсказка
 # - заводим переменную со значением 0, в которой будет храниться результат
 # - читаем каждую строку из файла (используем for - см. file-1.task.py в конце)
-# - переводим прочитанную строку в число и добавляем к результату
+# - переводим прочитанную строку в число и добавляем к результ
 
+file = open("data/olymp-003-in.txt", "r")
+
+s1 = file.readline()
+num1 = int(s1)
+
+s2 = file.readline()
+num2 = int(s2)
+
+s3 = file.readline()
+num3 = int(s3)
+
+result = num1 + num2 + num3
+res = str(result)
+
+file.close()
+
+file = open("tmp/olymp-003-out.txt", "w")
+
+file.write(res)
+
+file.close()
