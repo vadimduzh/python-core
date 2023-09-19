7f1a410d4a5642583b812556a21a2278984263b7
vduzh <vduzh@tut.by>
2023-09-19 11:22:03 +0300

    added olimp-003 olimp004

--- olymp-003.py
+++ olymp-003.py
@@ -6,14 +6,3 @@
 # - читаем каждую строку из файла (используем for - см. file-1.task.py в конце)
 # - переводим прочитанную строку в число и добавляем к результату
 
-res = 0
-
-file = open('data/olymp-003-in.txt', 'r')
-for s in file:
-    value = int(s)
-    res = res + value
-file.close()
-
-file = open('tmp/olymp-003-out.txt', "w")
-file.write(str(res))
-file.close()
