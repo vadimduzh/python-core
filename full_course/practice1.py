515de706d5d1a265a5e229524946df5b39152728
vadimduzh <Vadim.duzh@gmail.com>
2024-06-12 10:45:55 +0300

    Done

---
+++ full_course/practice1.py
@@ -0,0 +1,64 @@
+print('Vadim')
+print(7)
+list1 = [1, 2, 3]
+dic1 = {'apple':1, 'banano': 2, 'grape': 3}
+
+
+# встроенные функции в пайтоне
+
+
+
+# (функия) (вызов функции)
+print("Hello python")
+        # значение типа str
+
+
+# чтобы создать свою функцию следаует использовать def
+
+#   (имя) (параметры)
+def my_fn(a,b):
+    a = a + 1       # тело функции
+    c = a + b
+    return c
+#    (возврат результата)
+
+
+name = "vadim"
+#           метод
+print(name.upper())
+
+print(10, True, "Vadim")
+print(input)
+
+print(dir(__builtins__))
+
+
+#name = input("Enter ur name; ")
+#age = int(input("Enter ur age: "))
+#place = input("Enter the place where u live: ")
+
+
+#print(name, age, place)
+
+
+my_list = [10, 20, 30, 40, 50]
+
+n = my_list[3]
+
+my_list[3] = my_list[0]
+
+my_list[0] = n
+
+print(my_list)
+
+
+def func1(a, b):
+    a = b + 1
+    c = b + 2
+    return c
+
+
+
+
+
+
