7c3145117f0ed3df088aa8d7331f48d2a9b32554
vduzh <vduzh@tut.by>
2024-06-16 22:26:44 +0300

    added list tasks

---
+++ full_course/lesson_050.py
@@ -0,0 +1,32 @@
+# Дан список машин car_list.
+# Посчитать, сколько раз встречается в списке автомобиль Audi и сохранить в переменную audi_count.
+# Распечатать audi_count и убедиться, что вывелось 3.
+german_cars = ['Audi', 'BMW', 'Opel', 'Mercedes', 'Audi', 'Audi']
+
+
+# Дан список телефонов phone_lst.
+# Добавит в список телефон Xiaomi вторым элементом списка
+# Распечатать phone_lst и убедиться, что вывелось ['IPhone', 'Xiaomi',  'Samsung'].
+phone_lst = ['IPhone', 'Samsung']
+
+
+# Дан список num_lst.
+# Удалить все элементы списка.
+# Распечатать num_lst и убедиться, что вывелось [].
+num_lst = [10, 2, 17]
+
+# Даны два списка машин car_list and japanese_cars.
+# Добавить japanese_cars в список car_list
+# Распечатать car_list и убедиться, что вывелось ['Audi', 'BMW', 'Mazda'].
+car_list = ["Audi", "BMW"]
+japanese_cars = ["Mazda"]
+
+# Дан список age_lst.
+# Создать копию age_lst и сохранить в new_age_list
+# Распечатать new_age_list и убедиться, что вывелось [16, 16, 23].
+age_lst = [16, 16, 23]
+
+
+
+
+
