2e26c0f7fcab895e189dfd1085caeb7a9ebbff97
vduzh <vduzh@tut.by>
2024-06-16 22:46:57 +0300

    updated tasks

--- full_course/lesson_049.py
+++ full_course/lesson_049.py
@@ -1,13 +1,15 @@
-# Даны два списка машин german_cars.
-# С помощью среза (т.е. :) скопировать все элементы списка в новы список с именем all_cars
+# Даны список машин german_cars.
+# С помощью среза (т.е. :) скопировать все элементы списка german_cars в новый список с именем all_cars
 # Распечатать all_cars и убедиться, что вывелось ['Audi', 'BMW', 'Opel', 'Mercedes'].
 german_cars = ['Audi', 'BMW', 'Opel', 'Mercedes']
 all_cars = german_cars[:]
 print(all_cars)
 
-# Даны два списка машин phone_lst.
-# С помощью метода списка append скопировать все элементы списка в новы список с именем my_phone_lst.
+# Даны список телефонов phone_lst и пустой список my_phone_lst.
+# С помощью метода списка append скопировать все элементы из списка phone_lst в список my_phone_lst.
 # Распечатать my_phone_lst и убедиться, что вывелось ['IPhone', 'Samsung'].
 phone_lst = ['IPhone', 'Samsung']
+my_phone_lst = []
+
 my_phone_lst = phone_lst.copy()
 print(my_phone_lst)
