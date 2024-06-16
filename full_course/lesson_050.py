286e7abbfd1847f69d80dd4bf005e8d62fe98982
vadimduzh <Vadim.duzh@gmail.com>
2024-06-16 23:02:53 +0300

    Done

--- full_course/lesson_050.py
+++ full_course/lesson_050.py
@@ -2,30 +2,37 @@
 # Посчитать, сколько раз встречается в списке автомобиль Audi и сохранить в переменную audi_count.
 # Распечатать audi_count и убедиться, что вывелось 3.
 german_cars = ['Audi', 'BMW', 'Opel', 'Mercedes', 'Audi', 'Audi']
-
+audi_count = german_cars.count("Audi")
+print(audi_count)
 
 # Дан список телефонов phone_lst.
 # Добавит в список телефон Xiaomi вторым элементом списка
 # Распечатать phone_lst и убедиться, что вывелось ['IPhone', 'Xiaomi',  'Samsung'].
 phone_lst = ['IPhone', 'Samsung']
-print(phone_lst.insert(1, "Xiaomi"))
+phone_lst.insert(1, "Xiaomi")
+print(phone_lst)
 
 # Дан список num_lst.
 # Удалить все элементы списка.
 # Распечатать num_lst и убедиться, что вывелось [].
 num_lst = [10, 2, 17]
-print(num_lst.clear())
+num_lst.clear()
+print(num_lst)
 
 # Даны два списка машин car_list and japanese_cars.
 # Добавить japanese_cars в список car_list
 # Распечатать car_list и убедиться, что вывелось ['Audi', 'BMW', 'Mazda'].
 car_list = ["Audi", "BMW"]
 japanese_cars = ["Mazda"]
+car_list = car_list + japanese_cars
+print(car_list)
 
 # Дан список age_lst.
 # Создать копию age_lst и сохранить в new_age_list
 # Распечатать new_age_list и убедиться, что вывелось [16, 16, 23].
 age_lst = [16, 16, 23]
+new_age_list = age_lst.copy()
+print(new_age_list)
 
 
 
