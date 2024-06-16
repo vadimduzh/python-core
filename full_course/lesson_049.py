3d4b22b188c910bd7b204733867420bb5bdbe7e6
vduzh <vduzh@tut.by>
2024-06-16 12:07:16 +0300

    added list tasks

---
+++ full_course/lesson_049.py
@@ -0,0 +1,12 @@
+# Даны два списка машин german_cars.
+# С помощью среза (т.е. :) скопировать все элементы списка в новы список с именем all_cars
+# Распечатать all_cars и убедиться, что вывелось ['Audi', 'BMW', 'Opel', 'Mercedes'].
+german_cars = ['Audi', 'BMW', 'Opel', 'Mercedes']
+
+
+# Даны два списка машин phone_lst.
+# С помощью метода списка append скопировать все элементы списка в новы список с именем my_phone_lst.
+# Распечатать my_phone_lst и убедиться, что вывелось ['IPhone', 'Samsung'].
+phone_lst = ['IPhone', 'Samsung']
+
+
