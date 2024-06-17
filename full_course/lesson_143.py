90b791c8049802539c120c7e795edc33421aea44
vadimduzh <Vadim.duzh@gmail.com>
2024-06-17 15:21:07 +0300

    Done

--- full_course/lesson_143.py
+++ full_course/lesson_143.py
@@ -1,10 +1,26 @@
 # Используя if elif напиши программу, которая запрашивает у пользователя число и выводит, является ли оно положительным,
 # отрицательным или равным нулю.
-
+num = int(input("Enter the num: "))
+if num > 0:
+    print("This is positive num")
+elif num == 0:
+    print("This is zero")
+else:
+    print("This is negative num")
 
 # Напиши программу, которая запрашивает у пользователя номер месяца (от 1 до 12) и записывает в переменную season
 # соответствующее время года (зима, весна, лето, осень).
 # После выхода из if распечатать значение переменной season
+season = int(input("Enter the num of the month(from 1 to 12): "))
+if 0 < season < 4:
+    print("This is Winter")
+elif 3 < season < 7:
+    print("This is Spring")
+elif 6 < season < 10:
+    print("This is Summer")
+elif 9 < season <= 12:
+    print("This is Fall")
+
 
 
 # Напиши программу, которая запрашивает у пользователя два целых числа и выводит большее из них.
