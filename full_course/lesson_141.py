7060b73253b948519d35f1f67ade129db15ee556
vadimduzh <Vadim.duzh@gmail.com>
2024-06-17 22:39:58 +0300

    Done

--- full_course/lesson_141.py
+++ full_course/lesson_141.py
@@ -2,18 +2,15 @@
 # Если положительное, то вывести значение на экран и рядом написать Положительное
 first_num = 10
 if first_num > 0:
-    # TODO: а где "вывести значение на экран"
-    print("This num is positive")
+    print(first_num, "This num is positive")
 
 # Для заданного числа second_num проверить, является ли оно положительным.
 # Если отрицательное, то вывести значение на экран и рядом написать Отрицательное.
 second_num = -5
 if second_num > 0:
-    # TODO: а где "вывести значение на экран"
-    print("This is positive num")
+    print(second_num, "This is positive num")
 else:
-    # TODO: а где "вывести значение на экран"
-    print("This num is negative")
+    print(second_num, "This num is negative")
 
 
 
