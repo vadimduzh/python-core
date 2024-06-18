dcda74d1163c112152983e0f619731a7dd030a1f
vadimduzh <Vadim.duzh@gmail.com>
2024-06-18 22:36:31 +0300

    Done

--- full_course/lesson_152.py
+++ full_course/lesson_152.py
@@ -2,3 +2,6 @@
 # Создать цикл по списку user_lst, где user_name будет название переменной, что создается в цикле.
 # Распечатать user_name в теле цикла.
 # Убедиться, что будут распечатаны john, sam and nick каждый с новой строки.
+user_lst = ["john", "sam", "nick"]
+for user_name in user_lst:
+    print(user_name)
