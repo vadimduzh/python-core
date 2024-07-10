4afbf0e59ec04c8ce63e8358f739d8a16264fda4
vadimduzh <Vadim.duzh@gmail.com>
2024-07-10 18:18:01 +0300

    Done

--- olymp/factorial.py
+++ olymp/factorial.py
@@ -4,4 +4,12 @@
 # Факториал числа N = 5 считается как  5 * 4 * 3 * 2 * 1 и равен 120.
 #
 # Подсказка: можно использовать цикл for и диапазон range.
-#
+num = int(input("Enter the number >= 0: "))
+count = 1
+if num > 0:
+    for N in range(1, num + 1):
+        count = count * N
+    print(count)
+else:
+    print(1)
+
