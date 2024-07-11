608ca6a7eb5ca28591120d7ad5b6daa295039077
vadimduzh <Vadim.duzh@gmail.com>
2024-07-11 23:35:41 +0300

    done

--- olymp/find_second_max.py
+++ olymp/find_second_max.py
@@ -7,16 +7,22 @@ numbers_str = input("Enter the list of numbers: ")
 str_num_lst = numbers_str.split()
 print(str_num_lst)
 
-max_num = int(str_num_lst[0])
-second_max_num = int(str_num_lst[0])
-
+max_1 = int(str_num_lst[0])
 for str_num in str_num_lst:
     int_value = int(str_num)
-    if int_value > max_num:
-        max_num = int_value
-for second_str_num in str_num_lst:
-    second_int_value = int(second_str_num)
-    if second_int_value > second_max_num and second_int_value < max_num:
-        second_max_num = second_int_value
 
-print(second_max_num)
+    if int_value > max_1:
+        max_1 = int_value
+
+max_2 = None
+for str_num in str_num_lst:
+    int_value = int(str_num)
+
+    if int_value == max_1:
+        continue
+
+    if max_2 is None or max_2 < int_value:
+        max_2 = int_value
+
+print(max_2)
+
