26f7306bf6376a7dcadf94c192e528952f4f40da
vadimduzh <Vadim.duzh@gmail.com>
2024-07-11 15:45:46 +0300

    done

--- olymp/calculate_average.py
+++ olymp/calculate_average.py
@@ -1,3 +1,16 @@
 # Напишите программу, которая запрашивает у пользователя список чисел, разделенных пробелами, и вычисляет их
 # среднее арифметическое.
 # Убедиться, что 1 2 3 4 5 среднее арифметическое: 3.0
+numbers_str = input("Enter the list of numbers: ")
+
+str_num_lst = numbers_str.split()
+print(str_num_lst)
+
+sum = 0
+for str_num in str_num_lst:
+    int_value = int(str_num)
+    sum = sum + int_value
+
+average_value = sum / len(str_num_lst)
+print(average_value)
+
