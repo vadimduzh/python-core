09a4898766baaff3cab806d78ff9a44cd179362e
vadimduzh <Vadim.duzh@gmail.com>
2024-07-11 13:22:21 +0300

    done

--- olymp/find_max.py
+++ olymp/find_max.py
@@ -2,4 +2,16 @@
 # наибольшее число в этом списке.
 #
 # Подсказка: см. count_even_odd.py - там есть как разобрать строку.
+numbers_str = input("Enter the list of numbers: ")
 
+str_num_lst = numbers_str.split()
+print(str_num_lst)
+
+max_num = int(str_num_lst[0])
+
+for str_num in str_num_lst:
+    int_value = int(str_num)
+    if int_value > max_num:
+        max_num = int_value
+
+print(max_num)
