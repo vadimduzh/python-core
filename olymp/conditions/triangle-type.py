45e391cf5d3848b09e158335f2f25daaf563cb63
vadimduzh <Vadim.duzh@gmail.com>
2024-07-29 20:03:40 +0300

    done

--- olymp/conditions/triangle-type.py
+++ olymp/conditions/triangle-type.py
@@ -24,13 +24,13 @@ a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 c = int(input("Enter c: "))
 
-if a ** 2 == (b ** 2 + c ** 2):
+if (a ** 2 == (b ** 2 + c ** 2)) or (b ** 2 == (a ** 2 + c ** 2)) or (c ** 2 == (b ** 2 + a ** 2)):
     print("right")
 
-elif a ** 2 > (b ** 2 + c ** 2):
+elif (a ** 2 > (b ** 2 + c ** 2)) or (b ** 2 > (a ** 2 + c ** 2)) or (c ** 2 > (b ** 2 + a ** 2)):
     print("obtuse")
 
-elif a ** 2 < (b ** 2 + c ** 2):
+elif (a ** 2 < (b ** 2 + c ** 2)) or (b ** 2 < (a ** 2 + c ** 2)) or (c ** 2 < (b ** 2 + a ** 2)):
     print("acute")
 
 else:
