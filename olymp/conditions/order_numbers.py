505684c3230c169c846b79f67634f571edfb2e89
vadimduzh <Vadim.duzh@gmail.com>
2024-07-31 18:57:54 +0300

    done

--- olymp/conditions/order_numbers.py
+++ olymp/conditions/order_numbers.py
@@ -18,3 +18,40 @@
 # 1
 # выходные данные
 # 1 1 2
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+# num_lst = [a, b, c]
+# max_num = num_lst[0]
+# min_num = num_lst[0]
+# middle_num = num_lst[0]
+#
+# for i in num_lst:
+#     if i > max_num:
+#         max_num = i
+#     if i < min_num:
+#         min_num = i
+#
+# for i in num_lst:
+#     if max_num > i > min_num:
+#         middle_num = i
+#
+# print(min_num, middle_num, max_num)
+l = None
+if a > b:
+    l = a
+    a = b
+    b = l
+
+if a > c:
+    l = a
+    a = c
+    c = l
+
+if b > c:
+    l = b
+    b = c
+    c = l
+
+print(a, b, c)
