695312b6eb375453323a60c280d8a8ce6f00308f
vadimduzh <Vadim.duzh@gmail.com>
2024-07-31 19:22:58 +0300

    done

--- full_course/work.py
+++ full_course/work.py
@@ -1,2 +1,12 @@
-rate = 0.78
-print(round(rate))
+def simple_sort(lst):
+    for i in range(len(lst)):
+        for j in range(i + 1, len(lst)):
+            if lst[i] > lst[j]:
+                value = lst[i]
+                lst[i] = lst[j]
+                lst[j] = value
+    return lst
+
+
+res = simple_sort([1, 3, 2, 14, 6, 12, 4, 22, 0])
+print(res)
