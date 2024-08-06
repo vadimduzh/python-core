32f0e7b7e9471a91b7c86df3874d25049389c92b
vadimduzh <Vadim.duzh@gmail.com>
2024-08-06 21:52:05 +0300

    done

--- olymp/loops/even_numbers.py
+++ olymp/loops/even_numbers.py
@@ -13,3 +13,10 @@
 # выходные данные
 # 2 4
 
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+for i in range(a, b + 1):
+    if i % 2 == 0:
+        print(i)
+
--- olymp/loops/reminder.py
+++ olymp/loops/reminder.py
@@ -13,3 +13,11 @@
 # 2
 # выходные данные
 # 2 4
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+d = int(input("Enter d: "))
+
+for i in range(a, b + 1):
+    if i % d == c:
+        print(i)
