d86ee0dc5789acf2ae35668367f74a12573722f5
vadimduzh <Vadim.duzh@gmail.com>
2024-08-07 22:33:56 +0300

    done

--- olymp/loops/rotate_number.py
+++ olymp/loops/rotate_number.py
@@ -18,8 +18,30 @@
 # 12300
 # Выходные данные
 # 321
+x = input("Enter x: ")
 
+# решение один
+num = int(x)
+lst = []
 
+for i in range(len(x)):
+    rem = num % 10
+    lst.append(str(rem))
+    num = num // 10
 
+s = "".join(lst)
+res = int(s)
+print(res)
 
+# решение два
+s_1 = ""
+for el in x:
+    s_1 = el + s_1
 
+res_1 = int(s_1)
+print(res_1)
+
+# решение три
+s_2 = x[::-1]
+res_2 = int(s_2)
+print(res_2)
