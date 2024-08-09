73cd2a21f3f39f75f7c3f401a628fc31d3789428
vadimduzh <Vadim.duzh@gmail.com>
2024-08-09 16:44:24 +0300

    done

--- olymp/loops/equation-roots_dec.py
+++ olymp/loops/equation-roots_dec.py
@@ -6,3 +6,16 @@
 # Выходные данные
 # Найдите все целые решения уравнения ax**3 + bx**2 + cx + d = 0 на отрезке [0,1000] и выведите их в порядке убывания.
 # Если на данном отрезке нет ни одного решения, то ничего выводить не нужно.
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+d = int(input("Enter d: "))
+
+lst = []
+for x in range(0, 1000 + 1):
+    if a * x ** 3 + b * x ** 2 + c * x + d == 0:
+        lst.append(x)
+
+res = lst.sort(reverse=True)
+print(lst)
+
