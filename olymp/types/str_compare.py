4798d4ea31cb9ccceb2fb2bf49cabe1fe4baafda
vadimduzh <Vadim.duzh@gmail.com>
2024-08-25 11:48:20 +0300

    done

--- olymp/types/str_compare.py
+++ olymp/types/str_compare.py
@@ -20,6 +20,30 @@
 # yes
 s_1 = input("Enter the first symbol: ")
 s_2 = input("Enter the second symbol: ")
+count = 0
 
 if s_1 == s_2:
-    print("yes")
+    print(1, "yes")
+else:
+    print(1, "no")
+
+if len(s_1) != len(s_2):
+    print(2, "no")
+else:
+    # all
+    res = True
+    for i in range(len(s_1)):
+        res = res and (s_1[i] == s_2[i])
+        if not res:
+            break
+    print(4, res)
+
+#     for i in range(len(s_1)):
+#         if s_1[i] != s_2[i]:
+#             print(2, "no")
+#             break
+#         if s_1[i] == s_2[i]:
+#             count += 1
+#
+# if count == len(s_1):
+#     print(2, "yes")
